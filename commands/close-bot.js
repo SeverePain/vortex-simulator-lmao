eb2275db4b67e93fe040c1732b3d59eccced8a46
severepain <47048628+SeverePain@users.noreply.github.com>
2019-11-03 15:30:38 -0800

    Update close-bot.js

--- commands/close-bot.js
+++ commands/close-bot.js
@@ -3,7 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 if(message.author.id == bot.user.id){
+        let m = await message.reply(`:no_entry: Closing Bot!:`)
 	console.log(`[BOT] Close command registered! Closing Bot!`)
+setTimeout(function(){
+m.edit(":white_check_mark: Bot closed!")
+}, 3000)
 	process.exit()
 }
 else{
